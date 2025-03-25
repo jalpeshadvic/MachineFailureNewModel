2ed3413a78ace786735e395b311d4378026edac9
jalpeshadvic <jalpeshp@advic.io>
2025-03-25 12:41:28 +0530

    New Model

---
+++ app.py
@@ -0,0 +1,62 @@
+import streamlit as st
+import numpy as np
+import joblib
+
+# Load saved model
+model = joblib.load("model_rfcModel.pkl")
+# scaler = joblib.load("scaler.pkl")
+
+# Set Streamlit page layout
+st.set_page_config(page_title="Machine Failure Prediction", layout="wide")
+
+# Title
+st.markdown("<h1 style='text-align: center;'>Machine Failure Prediction</h1>", unsafe_allow_html=True)
+
+# Main input section (centered)
+with st.form("prediction_form"):
+    st.subheader("Enter Machine Parameters")
+
+    # Type selection (first column)
+    
+
+    col1, col2 = st.columns(2)  # Creating two columns for layout
+
+    with col1:
+        temperature = st.slider("Temperature", 35.55, 121.94)
+        vibration = st.slider("Vibration", -17.9, 113.8)
+        humidity = st.slider("Humidity", 30.0, 80.0)
+    
+    with col2:
+        pressure = st.slider("Pressure", 1,5)
+        energy_consumption = st.slider("Energy Consumption", 0.5, 5.0)
+        machine_status = st.selectbox("Machine Status", (0,1,2))
+
+    # Predict button
+    predict_button = st.form_submit_button("Predict Failure")
+
+# Prediction logic
+if predict_button:
+    input_data = np.array([[temperature, vibration, humidity, pressure, energy_consumption, machine_status]])
+
+    # Apply MinMaxScaler transformation
+    # input_data_scaled = scaler.transform(input_data)
+
+    # Predict using the ML model
+    prediction = model.predict(input_data)
+
+    # Display result
+    failure_types = {
+        0: "No Failure",
+        1: "Heat Dissipation Failure",
+        2: "Power Failure",
+        3: "Overstrain Failure",
+        4: "Tool Wear Failure",
+        5: "Random Failure"
+    }
+
+    result = failure_types.get(prediction[0], "Unknown Failure")
+
+    if prediction[0] == 0:
+        st.success(f"No Failure Predicted ✅")
+    else:
+        st.error(f"Machine will Fail.⚠️")
